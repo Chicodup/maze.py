4b1abc3f052ea6331fdccaa693169d0997f39b4a
Chicodup <140813799+Chicodup@users.noreply.github.com>
2024-05-12 17:30:16 +0300

    sa
    sa

--- maze.py
+++ maze.py
@@ -1,13 +1,17 @@
+from random import choice
 from pygame import *
-
 init()
+font.init()
+font1 = font.SysFont("Arial",100,True)
+game_over_text = font1.render("Game Over", True,(255,0,0))
+game_win_text_over_text = font1.render("WIN", True,(0,255,0))
 mixer.init()
 mixer.music.load('jungles.ogg')
 #mixer.music.play()
 mixer_music.set_volume(0.2)
 #створи вікно гри;
 MAP_WIDTH, MAP_HEIGHT = 25,20
-TILESIZE = 40
+TILESIZE = 33
 WIDTH,HEIGHT =MAP_WIDTH*TILESIZE, MAP_HEIGHT*TILESIZE
 window = display.set_mode((WIDTH,HEIGHT))
 FPS = 60
@@ -15,10 +19,10 @@ clock = time.Clock()
 #задай фон сцени
 bg = image.load('background.jpg')
 bg = transform.scale(bg,(MAP_WIDTH*TILESIZE, MAP_HEIGHT*TILESIZE))
+cyborg_img = image.load("treasure.png")
 player_img = image.load("hero.png")
 wall_img = image.load("wall.png")
-player_img2 = image.load("cyborg.png")
-gold_img = image.load("treasure.png")
+gold_img = image.load("cyborg.png")
 all_sprites = sprite.Group()
 
 #створи 2 спрайти та розмісти їх на сцені
@@ -29,15 +33,17 @@ class Sprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.mask = mask.from_surface(self.image)
         all_sprites.add(self)
 
 class Player(Sprite):
     def __init__(self,sprite_img,width,height,x,y):
         super().__init__(sprite_img,width,height,x,y)
         self.hp = 100
-        self.speed = 4
+        self.speed = 6
     def update(self):
         key_pressed = key.get_pressed()
+        old_pos = self.rect.x, self.rect.y
         if key_pressed[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
         if key_pressed[K_s]and self.rect.bottom < HEIGHT:
@@ -47,10 +53,41 @@ class Player(Sprite):
         if key_pressed[K_d]and self.rect.right < WIDTH:
             self.rect.x += self.speed
 
-        
-player = Player(player_img,TILESIZE,TILESIZE,300,300)
-player2 = Player(player_img2,TILESIZE,TILESIZE,45,42)
+        collide_list = sprite.spritecollide(self,walls,False,sprite.collide_mask)
+        if len(collide_list) > 0:
+            self.rect.x, self.rect.y = old_pos
+        enemy_collide = sprite.spritecollide(self,enemys,False,sprite.collide_mask)
+        if len(enemy_collide) > 0:
+            self.hp -= 100
+
+class Enemy(Sprite):
+    def __init__(self,sprite_img,width,height,x,y):
+        super().__init__(sprite_img,width,height,x,y)
+        self.damage = 1
+        self.speed = 1
+        self.dir_list = ["right","left","up","down"]
+        self.dir = choice(self.dir_list)
+    def update(self):
+        old_pos = self.rect.x, self.rect.y
+        if self.dir == "right":
+            self.rect.x += self.speed
+        elif self.dir == "left":
+            self.rect.x -= self.speed
+        elif self.dir == "up":
+            self.rect.y -= self.speed
+        elif self.dir == "down":
+            self.rect.y += self.speed
+        collide_list = sprite.spritecollide(self,walls,False,sprite.collide_mask)
+        if len(collide_list) > 0:
+            self.dir = choice(self.dir_list)
+            self.rect.x, self.rect.y = old_pos
+
+player = Player(player_img,TILESIZE-5,TILESIZE-5,300,300)
 walls = sprite.Group()
+enemys = sprite.Group()
+
+
+
 
 with open("map.txt", "r") as f:
     map = f.readlines()
@@ -64,21 +101,35 @@ with open("map.txt", "r") as f:
                 player.rect.x = x
                 player.rect.y = y
             if symwol == "g":
-                walls.add(Sprite(gold_img, 70,70,x,y))
+                gold = Sprite(gold_img, 70,70,x,y)
+            if symwol == "e":
+                enemys.add(Enemy(cyborg_img, TILESIZE,TILESIZE,x,y))
             x += TILESIZE
         y+=TILESIZE
         x = 0
 
 #оброби подію «клік за кнопкою "Закрити вікно"»
 run = True
+finish = False
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+            if player.hp <= 0:
+                run = False
     
     window.blit(bg, (0,0))
+    if player.hp <= 0:
+        finish = True
+    if sprite.collide_mask(player,gold):
+        finish = True
+        game_over_text = font1.render("WIN", True,(0,255,0))
+
     all_sprites.draw(window)
-    all_sprites.update()
+    if not finish:
+        all_sprites.update()
+    if finish:
+        window.blit(game_over_text,(230,270))
 
     display.update()
     clock.tick(FPS)
